e1a95f34b35f28b43019383a16d3593f6844bc44
Chris Lemler <chris@lemler.me>
2021-10-20 22:31:26 -0700

    Added file logic to open the src and dest files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use text_colorizer::*;
 use std::env;
+use std::fs;
 
 #[derive(Debug)]
 struct Arguments {
@@ -40,5 +41,22 @@ fn parse_args() -> Arguments {
 
 fn main() {
     let args = parse_args();
-    println!("{:?}", args);
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to read from file '{}' : {:?}",
+                "Error:".red().bold(), args.filename, e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, data) {
+        Ok(_) => {},
+        Err(e) => {
+            eprintln!("{} failed to write to file '{}' : {:?}",
+                "Error:".red().bold(), args.output, e);
+            std::process::exit(1);
+        }
+    };
 }
